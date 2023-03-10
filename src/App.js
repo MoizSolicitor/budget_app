640faed5231ca38c1725a0da51ab03f04323774a
moizprogrammer <moizprogrammer24@gmail.com>
2023-03-10 21:35:49 +0500

    'commit'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container,Stack,Button } from "react-bootstrap";
+import AddBudgetModal from "./components/AddBudgetModal";
+import BudgetCard from "./components/BudgetCard";
+import { useState } from "react";
+import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "./contexts/BudgetsContext";
+import AddExpenseModal from "./components/AddExpenseModal";
+import UncategorizedBudgetCard from "./components/UncategorizedBudgetCard";
+import TotalBudgetCard from "./components/TotalBudgetCard";
+import ViewExpensesModal from "./components/ViewExpensesModal";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [showAddBudgetModal,setShowAddBudgetModal]=useState(false)
+    const [showAddExpenseModal, setShowAddExpenseModal] = useState(false)
+    const [viewExpensesModalBudgetId, setViewExpensesModalBudgetId] = useState()
+    const [addExpenseModalBudgetId, setAddExpenseModalBudgetId] = useState()
+    const {budgets,getBudgetExpenses}=useBudgets()
+
+    function openAddExpenseModal(budgetId){
+       setShowAddExpenseModal(true)
+        addExpenseModalBudgetId(budgetId)
+    }
+
+ return (
+  <>
+ <Container className="my-4">
+  <Stack direction="horizontal" gap='2' className="mb-4">
+ <h1 className="me-auto">Budget</h1>
+ <Button variant='primary' onClick={()=>setShowAddBudgetModal(true)}>Add Budget</Button>
+ <Button variant='outline-primary' onClick={openAddExpenseModal}>Add Expense</Button>
+  </Stack>
+  <div style={{display:'grid',gridTemplateColumns:'repeat(auto-fill,minmax(300px,1fr))',gap:'1rem',alignItems:'flex-start'}}>
+    {budgets.map(budget=>{
+        const amount=getBudgetExpenses(budget.id).reduce((total,expense)=>total +expense.amount,0)
+      return ( 
+              <BudgetCard  name={budget.name} key={budget.id} amount={amount} max={budget.max} onAddExpenseClick={(()=>openAddExpenseModal(budget.id))} onViewExpensesClick={()=>setViewExpensesModalBudgetId(budget.id)}
+              />
+      )  
+})}
+
+<UncategorizedBudgetCard onAddExpenseClick={openAddExpenseModal}  onViewExpensesClick={()=>setViewExpensesModalBudgetId(UNCATEGORIZED_BUDGET_ID)}/>
+<TotalBudgetCard/>
+      
+  </div>
+ </Container>
+ <AddBudgetModal show={showAddBudgetModal} handleClose={()=>setShowAddBudgetModal(false)}/>
+ <AddExpenseModal show={showAddExpenseModal} handleClose={()=>setShowAddExpenseModal(false)} defaultBudgetId={addExpenseModalBudgetId}/>
+ <ViewExpensesModal budgetId={viewExpensesModalBudgetId} handleClose={()=>setViewExpensesModalBudgetId()}/>
+
+ </>
+ )
+ 
+ 
 }
 
 export default App;
